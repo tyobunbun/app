32ae041a5557a3822c40c2be761ef0a8e6b41fbb
zhang <zhangwenwen@kanrinoMacBook-puro.local>
2018-08-03 11:13:00 +0900

    Active Storage

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -71,6 +71,6 @@ class MicropostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content, :image)
     end
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 140 }
+  has_one_attached :image
                       
 end
